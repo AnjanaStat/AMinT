d549778bbf259158f2ca8f145a77406d5c469952
AnjanaStat <107476002+AnjanaStat@users.noreply.github.com>
2022-06-28 13:32:32 +0530

    Initial commit

---
+++ R/AMinT.R
@@ -0,0 +1,11 @@
+#' Find test statistic value from a given data and corresponding critical value
+#'
+#' This package finds test statistic value and
+#' critical value of asymptotic Min-T test for testing equality of means aginst
+#' ordered alternatives in one-way ANOVA model with heteroscedasticity.
+#'
+#' @docType package
+#'
+#' @author Anjana Mondal \email{mondal.anjana25@gmail.com}
+#'
+#' @name AMinT
---
+++ README.md
@@ -0,0 +1,2 @@
+# AMinT
+ This package finds test statistic value and critical value of parametric bootstrap Min-T test for testing equality of treatment effects against strictly increasing ordered alternatives in one-way ANOVA model with heteroscedasticity.
